5f29cd41fbda25b7f5b42e7c4d050268a2324260
Bumdai <vanaslepcov861@gmail.com>
2026-03-17 02:33:51 +1000

    add 5 Servises, 5Decorators, 5AIControllers, 5BTTask

---
+++ Source/DroneAI/Private/Pawn/Enemies/Controllers/AIController05.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawn/Enemies/Controllers/AIController05.h"
+
+
+// Sets default values
+AAIController05::AAIController05()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAIController05::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAIController05::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/DroneAI/Public/Pawn/Enemies/Controllers/AIController05.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIController05.generated.h"
+
+UCLASS()
+class DRONEAI_API AAIController05 : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAIController05();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
